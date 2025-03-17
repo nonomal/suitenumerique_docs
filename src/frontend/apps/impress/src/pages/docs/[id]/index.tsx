b717fc6489d9fb097edc2e503f77b2af8cccc554
Nathan Panchout <nathanpanchout@hotmail.com>
2025-03-17 15:13:02 +0100

    ✨(frontend) added subpage management and document tree features
    New components were created to manage subpages in the document tree,
    including the ability to add, reorder, and view subpages. Tests were
    added to verify the functionality of these features. Additionally, API
    changes were made to manage the creation and retrieval of document
    children.

--- src/frontend/apps/impress/src/pages/docs/[id]/index.tsx
+++ src/frontend/apps/impress/src/pages/docs/[id]/index.tsx
@@ -6,14 +6,15 @@ import { useEffect, useState } from 'react';
 
 import { Box, Text, TextErrors } from '@/components';
 import { DocEditor } from '@/docs/doc-editor';
+import { KEY_AUTH, setAuthUrl } from '@/features/auth';
 import {
   Doc,
   KEY_DOC,
   useCollaboration,
   useDoc,
   useDocStore,
-} from '@/docs/doc-management/';
-import { KEY_AUTH, setAuthUrl } from '@/features/auth';
+} from '@/features/docs/doc-management/';
+import { DocTreeProvider } from '@/features/docs/doc-tree/context/DocTreeContext';
 import { MainLayout } from '@/layouts';
 import { useBroadcastStore } from '@/stores';
 import { NextPageWithLayout } from '@/types/next';
@@ -33,9 +34,11 @@ export function DocLayout() {
         <meta name="robots" content="noindex" />
       </Head>
 
-      <MainLayout>
-        <DocPage id={id} />
-      </MainLayout>
+      <DocTreeProvider initialTargetId={id}>
+        <MainLayout>
+          <DocPage id={id} />
+        </MainLayout>
+      </DocTreeProvider>
     </>
   );
 }
@@ -81,6 +84,14 @@ const DocPage = ({ id }: DocProps) => {
     setDoc(docQuery);
     setCurrentDoc(docQuery);
   }, [docQuery, setCurrentDoc, isFetching]);
+
+  useEffect(() => {
+    return () => {
+      console.log('unmount');
+      setCurrentDoc(undefined);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   /**
    * We add a broadcast task to reset the query cache
